156f7cd5b695b9d8b389179fd9cb88e648b68198
bongiweqanda87 <bongiweqanda87@gmail.com>
2026-09-14 20:32:44 +0200

    feat: add daily scheduler and pipeline logging

---
+++ scheduler.py
@@ -0,0 +1,29 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+from etl.fetch import fetch_etf_data
+from etl.transform import transform_raw_data
+from db.load import load_etf_data
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+def run_pipeline():
+    tickers = ["STX40.JO", "STXNDQ.JO","SYG4IR.JO"]
+
+    for ticker in tickers:
+        logger.info(f"Running pipeline for {ticker}")
+        raw_data = fetch_etf_data(ticker)
+        clean_data = transform_raw_data(raw_data)
+        load_etf_data(clean_data,ticker)
+        logger.info(f"Pipeline completed for {ticker}")
+
+if __name__ == "__main__":
+    scheduler = BlockingScheduler()
+    scheduler.add_job(run_pipeline, 'cron', hour=8, minute=0)
+    print("Scheduler started - pipeline runs daily at 08:00")
+    scheduler.start()
